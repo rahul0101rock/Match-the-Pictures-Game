d25e27d0e04f8612a282984b559f38130c532927
Rahul Choudhary <rahul0101rock@gmail.com>
2020-07-26 23:14:36 +0530

    Added Sound

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import pygame
 import game_config as gc 
 from animal import Animal
-from pygame import display,event,image
+from pygame import display,event,image,mixer
 from time import sleep
+import tkinter as tk
+from tkinter import messagebox
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 def findin(x,y):
     row=y//gc.imsize
     col=x//gc.imsize
     index=row*gc.numside+col
     return(index)
 pygame.init()
-display.set_caption("Memory Game By Rahul")
+mixer.init()
+mixer.music.set_volume(0.7)
+display.set_caption("Mermory Game By Rahul")
 screen=display.set_mode((512,512))
 matched =image.load('matched.png')
 run=True
@@ -19,10 +32,12 @@ while run:
     ev=event.get()
     for e in ev:
         if e.type==pygame.QUIT:
-            run=False
+            print("Game Ended")
+            pygame.quit()
         if e.type ==pygame.KEYDOWN:
             if e.key==pygame.K_ESCAPE:
-                run=False
+                print("Game Ended")
+                pygame.quit()
         if e.type==pygame.MOUSEBUTTONDOWN:
             mx,my=pygame.mouse.get_pos()
             index=findin(mx,my)
@@ -30,7 +45,6 @@ while run:
                 crimg.append(index)
             if len(crimg)>2:
                 crimg=crimg[1:]
-
     screen.fill((255,255,255))
     tskip=0
     for _,t in enumerate(tiles):
@@ -45,11 +59,19 @@ while run:
         if tiles[img1].name==tiles[img2].name:
             tiles[img1].skip=True
             tiles[img2].skip=True
+            mixer.music.load("tada.mp3")
+            mixer.music.play()
             sleep(0.4)
             screen.blit(matched,(0,0))
             display.flip()
             sleep(0.4)
+            mixer.music.stop()
             crimg=[]
     if tskip ==len(tiles):
         run=False
+mixer.music.load("applause.mp3")
+mixer.music.play()
+message_box("Congrats","Game Completed")
+mixer.music.stop()
+pygame.quit()
 print("Game Ended")
